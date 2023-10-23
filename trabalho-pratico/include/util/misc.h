c6d96b4e5eeaf48578e93f6d3ce8a7c58c73fb2d
DarkenLM <rafaelsantosfernandes660@gmail.com>
2023-10-23 12:20:46 +0100

    docs(util(io)): added internal note

--- trabalho-pratico/include/util/misc.h
+++ trabalho-pratico/include/util/misc.h
@@ -3,6 +3,7 @@
 
 #define MAX_BIT_VALUE(bits) ((1ULL << (bits)) - 1)
 
+// Internal usage. Use LITERALIZE instead.
 #define LITERALIZE_HELPER(token) #token
 #define LITERALIZE(token) LITERALIZE_HELPER(token)
 
