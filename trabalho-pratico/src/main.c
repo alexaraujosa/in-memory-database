a31fbe50cba37c787e72f4e395a29e18dbd1cf17
alexaraujosa <alexaraujosa@hotmail.com>
2023-11-12 02:52:21 +0000

    feat(main): added full main.c file. Needs a refactor of the code, but its just for the test platform

--- trabalho-pratico/src/main.c
+++ trabalho-pratico/src/main.c
@@ -7,6 +7,8 @@
 #include "parser/parser.h"
 #include "queries/queries.h"
 
+#include "parser/verifier.h"
+
 typedef struct test {
     char* id;
 } TEST, *Test;
@@ -30,21 +32,21 @@ int testVerifier(Tokens tokens) {
     // for (int i = 0; i < tokens->len; i++) printf("VERIFIER TOKENS %d: '%s'\n", i, tokens->data[i]);
     // if (++ind == 3) return 0; // FYIP
     // return 1;
-
     return 0;
 }
 
 void* testParser(Tokens tokens) {
-    Test test = (Test)malloc(sizeof(TEST));
-    test->id = strdup(tokens->data[0]);
+    // Test test = (Test)malloc(sizeof(TEST));
+    // test->id = strdup(tokens->data[0]);
 
-    return test;
+    // return test;
+    return;
 }
 
 void testWriter(void* raw_data, FILE** store) {
-    IGNORE_ARG(store);
-    Test data = (Test)raw_data;
-    printf("WRITE: %s\n", data->id);
+    // IGNORE_ARG(store);
+    // Test data = (Test)raw_data;
+    //printf("WRITE: %s\n", data->id);
     fflush(stdout);
 }
 
@@ -75,10 +77,10 @@ void testWriter(void* raw_data, FILE** store) {
 //     free(joint);
 // }
 
-void testDiscarder(void* raw_data, ParserStore store) {// , FILE** store
+void discard_user(void* raw_data, ParserStore store) {
     void** discard_file = &g_array_index(store, void*, 0);
     if (*discard_file == NULL) {
-        char* parts[2] = {get_cwd()->str, "../Resultados/errors.csv"};
+        char* parts[2] = {get_cwd()->str, "../Resultados/users_errors.csv"};
         char* full_path = join_paths(parts, 2);
         *discard_file = OPEN_FILE(full_path, "w");
 
@@ -89,14 +91,73 @@ void testDiscarder(void* raw_data, ParserStore store) {// , FILE** store
         );
 
         fputs(*file_header, *discard_file);
+        free(full_path);
     }
-    // if (store->discard_file == NULL) {
-    //     char* parts[2] = {get_cwd()->str, "../Resultados/errors.csv"};
-    //     char* full_path = join_paths(parts, 2);
-    //     store->discard_file = OPEN_FILE(full_path, "w");
+    Tokens tokens = (Tokens)raw_data;
 
-    //     fputs(store->file_header, store->discard_file);
-    // }
+    discard_to_file(tokens, store);
+}
+
+void discard_passenger(void* raw_data, ParserStore store) {
+    void** discard_file = &g_array_index(store, void*, 0);
+    if (*discard_file == NULL) {
+        char* parts[2] = {get_cwd()->str, "../Resultados/passengers_errors.csv"};
+        char* full_path = join_paths(parts, 2);
+        *discard_file = OPEN_FILE(full_path, "w");
+
+        void** file_header = &g_array_index(store, void*, 1);
+        rt_assert(
+            *file_header != NULL,
+            "Could not open discard file: Dataset header missing."
+        );
+
+        fputs(*file_header, *discard_file);
+        free(full_path);
+    }
+
+    Tokens tokens = (Tokens)raw_data;
+
+    discard_to_file(tokens, store);
+}
+
+void discard_flight(void* raw_data, ParserStore store) {
+    void** discard_file = &g_array_index(store, void*, 0);
+    if (*discard_file == NULL) {
+        char* parts[2] = {get_cwd()->str, "../Resultados/flights_errors.csv"};
+        char* full_path = join_paths(parts, 2);
+        *discard_file = OPEN_FILE(full_path, "w");
+
+        void** file_header = &g_array_index(store, void*, 1);
+        rt_assert(
+            *file_header != NULL,
+            "Could not open discard file: Dataset header missing."
+        );
+
+        fputs(*file_header, *discard_file);
+        free(full_path);
+    }
+
+    Tokens tokens = (Tokens)raw_data;
+
+    discard_to_file(tokens, store);
+}
+
+void discard_reservation(void* raw_data, ParserStore store) {
+    void** discard_file = &g_array_index(store, void*, 0);
+    if (*discard_file == NULL) {
+        char* parts[2] = {get_cwd()->str, "../Resultados/reservations_errors.csv"};
+        char* full_path = join_paths(parts, 2);
+        *discard_file = OPEN_FILE(full_path, "w");
+
+        void** file_header = &g_array_index(store, void*, 1);
+        rt_assert(
+            *file_header != NULL,
+            "Could not open discard file: Dataset header missing."
+        );
+
+        fputs(*file_header, *discard_file);
+        free(full_path);
+    }
 
     Tokens tokens = (Tokens)raw_data;
 
@@ -108,90 +169,78 @@ void testDestructor(FILE* stream, ParserStore store) {
 }
 
 int main(int argc, char const *argv[]) {
-    printf("Hello World!\n");
 
-    RESERVATION res = makeReservation(
-        1, 
-        "123456789012345678901234567890123456789012345678901234567890",
-        100,
-        "098765432109876543210987654321098765432109876543210987654321",
-        5,
-        4,
-        123456,
-        4321,
-        196,
-        false,
-        1
-    );
 
-    USER user = makeUser(
-        "123456789012345678901234567890123456789012345678901234567890",
-        "Manuel Maria Filipe Carlos Amélio Luís Miguel Rafael Gabriel Gonzaga Xavier "
-            "Francisco de Assis Eugénio de Bragança Orleães Sabóia e Saxe-Coburgo-Gotha",
-        false,
-        "PT",
-        12345,
-        true
-    );
+    // init_batch(recebe o argv[0] como argumento)
 
-    FLIGHT flight = makeFlight(
-        1,
-        "Fuckery Airlines",
-        "Fuckonov 69420",
-        "PQP",
-        "CDC",
-        12345,
-        23456,
-        34567
-    );
+    char* userdata_path_parts[2] = {"/mnt/data", "users.csv"};
+    char* userdata_path = join_paths(userdata_path_parts, 2);
 
-    GString* bin = get_cwd();
-    printf("BIN1: %s\n", bin->str);
+    char* flightsdata_path_parts[2] = {"/mnt/data", "flights.csv"};
+    char* flightsdata_path = join_paths(flightsdata_path_parts, 2);
 
-    GString* bin2 = get_cwd();
-    printf("BIN2: %s\n", bin2->str);
+    char* passengersdata_path_parts[2] = {"/mnt/data", "passengers.csv"};
+    char* passengersdata_path = join_paths(passengersdata_path_parts, 2);
 
-    char* parts1[2] = {bin->str, "../obj"};
-    char* full_path1 = join_paths(parts1, 2);
-    GArray* files = get_files(full_path1);
-    if (files == NULL || files->len == 0) {
-        printf("ALLAHU AKBAR: No files.\n");
-        exit(EXIT_FAILURE);
-    }
-    for (guint i = 0; i < files->len; i++) {
-        char *dirp = g_array_index(files, char*, i);
-        g_print("FILE: '%s'\n", dirp);
-    }
+    char* reservationsdata_path_parts[2] = {"/mnt/data", "reservations.csv"};
+    char* reservationsdata_path = join_paths(reservationsdata_path_parts, 2);
 
-    char* parts2[2] = {bin->str, "../obj"};
-    char* full_path2 = join_paths(parts2, 2);
-    GArray* subdirs = get_subdirs(full_path2);
-    if (subdirs == NULL || subdirs->len == 0) {
-        printf("ALLAHU AKBAR: No subdirs.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    for (guint i = 0; i < subdirs->len; i++) {
-        char *dirp = g_array_index(subdirs, char*, i);
-        g_print("SUBDIR: '%s'\n", dirp);
-    }
-
-    // parse("../test2.txt", &tokenize_csv, &testVerifier, &testParser, &testWriter, &testDiscarder);
     parse_file(
-        "../test2.txt", 
-        // "/home/rafaelsf/Desktop/Cadeiras/2ano/LI3/Project/project/trabalho-pratico/test2 copy.txt",
+        userdata_path,     
         &tokenize_csv,
         &testPreprocessor,
-        &testVerifier, 
+        &verify_user_tokens, 
         &testParser, 
         &testWriter, 
-        &testDiscarder,
+        &discard_user,
         &testDestructor
     );
 
+    parse_file(
+        passengersdata_path,
+        &tokenize_csv,
+        &testPreprocessor,
+        &verify_passenger_tokens, 
+        &testParser, 
+        &testWriter, 
+        &discard_passenger,
+        &testDestructor
+    );
+
+    parse_file(
+        reservationsdata_path,
+        &tokenize_csv,
+        &testPreprocessor,
+        &verify_reservation_tokens, 
+        &testParser, 
+        &testWriter, 
+        &discard_reservation,
+        &testDestructor
+    );
+
+    parse_file(
+        flightsdata_path,
+        &tokenize_csv,
+        &testPreprocessor,
+        &verify_flight_tokens, 
+        &testParser, 
+        &testWriter, 
+        &discard_flight,
+        &testDestructor
+    );
+
+    free(userdata_path);
+    free(passengersdata_path);
+    free(flightsdata_path);
+    free(reservationsdata_path);
+
+    g_free(get_cwd()->str);
+    g_free(get_cwd());
+
+
     // query_run_single("2 U000000001", 12);
-    query_run_bulk("../testq.txt", "ignore");
-    
+    // query_run_bulk("../testq.txt", "ignore");
+
     return 0;
 }
 
