d9aaef0c295047b7e49471e4dd4f021c8ca8ad69
alexaraujosa <alexaraujosa@hotmail.com>
2023-11-16 13:54:56 +0000

    docs(string): added string functions documentation

--- trabalho-pratico/include/util/string.h
+++ trabalho-pratico/include/util/string.h
@@ -25,21 +25,152 @@
 
 char* isnprintf(const char *format, ...);
 char* strdup_to(char* dest, char* src);
+
+/**
+ * @brief Verify if a certain char its a digit.
+ * 
+ * @param c Digit.
+ * 
+ * @return 1 if its a digit, otherwise 0.
+*/
 int is_digit(char c);
+
+/**
+ * @brief Verify if a certain char its a positive digit.
+ * 
+ * @param c Digit.
+ * 
+ * @return 1 if its a positive digit, otherwise 0.
+*/
 int is_digit_positive(char c);
+
+/**
+ * @brief Verify if a string contains a non negative number.
+ * 
+ * @param number String that contains the number.
+ * 
+ * @return 1 if it contains a non digit number, otherwise 0.
+*/
 int is_integer_positive_or_zero(char* number);
+
+/**
+ * @brief Verify if a string contains a positive number.
+ * 
+ * @param number String that contains the number.
+ * 
+ * @return 1 if it contains a positive digit number, otherwise 0.
+*/
 int is_integer_positive(char* number);
+
+/**
+ * @brief Verify if a string contains a number between 1 and 5.
+ * 
+ * @param number String that contains the number.
+ * 
+ * @return 1 if it contains a digit between 1 and 5, otherwise 0.
+*/
 int is_integer_between_one_and_five(char* number);
+
+/**
+ * @brief Verify if a matrix have white strings.
+ * 
+ * @param parameter Array of strings.
+ * @param number_parameters Number of strings.
+ * 
+ * @return 1 if it doesn't contains white strings, otherwise 0.
+*/
 int have_whitespace(char* parameter[], int num_parameters);
+
+/**
+ * @brief Verify if a string represent an email.
+ * 
+ * @param parameter String that contains the email.
+ * @param length Length of the string.
+ * 
+ * @return 1 if its an email, otherwise 0.
+*/
 int is_email(char* parameter, int length);
+
+/**
+ * @brief Verify if a string represent a boolean.
+ * 
+ * @param value String that contains the possible boolean.
+ * 
+ * @return 1 if it represents a boolean, otherwise 0.
+*/
 int is_boolean(char* value);
+
+/**
+ * @brief Verify a string length.
+ * 
+ * @param string String to be verified.
+ * @param length Possible length of the string.
+ * 
+ * @return 1 if it has that length or more, otherwise 0.
+*/
 int is_length(char* string, int length);
+
+/**
+ * @brief Verify if a string represents a date.
+ * 
+ * @param string String to be verified.
+ * 
+ * @return 1 if it represents, otherwise 0.
+*/
 int is_date(char* string);
+
+/**
+ * @brief Verify if a string represents a date with time.
+ * 
+ * @param string String to be verified.
+ * 
+ * @return 1 if it represents, otherwise 0.
+*/
 int is_date_with_time(char* string);
+
+/**
+ * @brief Get the sex gender from the string.
+ * 
+ * @param parameter String containing the gender.
+ * 
+ * @return TRUE if its Male, FALSE otherwise.
+*/
 bool get_sex(char* parameter);
+
+/**
+ * @brief Get the account status from the string.
+ * 
+ * @param parameter String containing the account status.
+ * 
+ * @return TRUE if its active, FALSE otherwise.
+*/
 bool get_account_status(char* parameter);
+
+/**
+ * @brief Turns a string that represents a date to an int.
+ * 
+ * @param parameter String containing the date.
+ * 
+ * @return Date in int.
+*/
 int date_string_to_int(char* parameter);
+
+/**
+ * @brief Turns a string that represents a date with time to an int.
+ * 
+ * @param parameter String containing the date with time.
+ * 
+ * @return Date with time in int.
+*/
 int date_with_time_string_to_int(char* parameter);
+
+/**
+ * @brief Get the boolean from the string.
+ * 
+ * @param parameter String containing the boolean.
+ * 
+ * @return TRUE if its true, FALSE otherwise.
+*/
 bool get_boolean(char* parameter);
 
 #endif
