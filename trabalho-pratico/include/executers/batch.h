13560ffbbb68f352f59f96b6b27775167e755282
alexaraujosa <alexaraujosa@hotmail.com>
2023-11-16 13:37:56 +0000

    docs(batch): added batch function documentation

--- trabalho-pratico/include/executers/batch.h
+++ trabalho-pratico/include/executers/batch.h
@@ -1,10 +1,6 @@
 #ifndef TP_EXECUTERS_BATCH_H
 #define TP_EXECUTERS_BATCH_H
 
-#include "collections/user.h"
-#include "collections/reservation.h"
-#include "collections/flight.h"
-#include "collections/passenger.h"
 #include "queries/queries.h"
 #include "catalog/userCatalog.h"
 #include "catalog/reservationsCatalog.h"
@@ -16,6 +12,14 @@ void test_preprocessor(FILE* stream, ParserStore store, va_list args);
 void test_parser(Tokens tokens);
 void test_writer(void* raw_data, FILE** store);
 
+/**
+ * @brief Program execution in batch mode.
+ * 
+ * @param arg1 Path to the csv files.
+ * @param arg2 Path to the file that contain the commands to execute queries.
+ * 
+ * @return void.
+*/
 void batch(const char* arg1, const char* arg2);
 
 #endif
