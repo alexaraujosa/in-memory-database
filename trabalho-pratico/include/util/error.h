8d6cbaa5e2f16ed11f3cf5731a00111c283f8a9f
DarkenLM <rafaelsantosfernandes660@gmail.com>
2023-12-31 22:25:20 +0000

    feat(util/error): added missing function required by commits up to e092854
    - Added missing function trace_msg

--- trabalho-pratico/include/util/error.h
+++ trabalho-pratico/include/util/error.h
@@ -2,6 +2,7 @@
 #define TP_UTIL_ERROR_H
 
 #include "common.h"
+#include "util/string.h"
 #include <assert.h>
 #include <errno.h>
 
@@ -9,9 +10,12 @@
 #ifndef NDEBUG
     #define m_assert(expr, msg) assert(( (void)(msg), (expr) ))
 #else
-    #define m_Assert(expr, msg) ;
+    #define m_assert(expr, msg) ;
 #endif
 
 void rt_assert(int condition, const char *message);
+// char* trace_msg(const char* scope, const char* message);
+
+#define trace_msg(scope, message) "ERROR [" scope "] " message
 
 #endif
