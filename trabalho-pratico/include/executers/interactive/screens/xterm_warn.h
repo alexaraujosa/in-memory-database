a6e2609b0e6c113269fe8ac497eb9981965a2865
DarkenLM <rafaelsantosfernandes660@gmail.com>
2024-01-14 15:53:16 +0000

    feat(exec/inter/screens/xterm_warn): [NOTICE] From now on, EIS is an alias for exec/inter/screens Updated screen xterm_warn to new screen management structure.
    - Removed mental sanity.
    
    BREAKING CHANGE: Text is now computed on the cache for the screen.

--- trabalho-pratico/include/executers/interactive/screens/xterm_warn.h
+++ trabalho-pratico/include/executers/interactive/screens/xterm_warn.h
@@ -4,11 +4,27 @@
 #include "executers/interactive/interactive_common.h"
 
 /**
+ * @brief Creates a cache for the XTerm Warn screen.
+ * 
+ * @param term The current GM_Term.
+ * @param store The current FrameStore.
+ */
+Cache make_cache_xterm_warn(GM_Term term, FrameStore store);
+
+/**
+ * @brief Destroys the cache for the XTerm Warn screen.
+ * 
+ * @param cache The Cache.
+ */
+void destroy_cache_xterm_warn(Cache cache);
+
+/**
  * @brief Draws the XTerm Warn screen.
  * 
  * @param term The current GM_Term.
+ * @param store The current FrameStore.
  */
-void draw_xterm_warn(GM_Term term);
+void draw_xterm_warn(GM_Term term, FrameStore store, Cache cache);
 
 
 #endif
--- trabalho-pratico/src/executers/interactive/screens/xterm_warn.c
+++ trabalho-pratico/src/executers/interactive/screens/xterm_warn.c
@@ -1,20 +1,68 @@
 #include "executers/interactive/interactive_common.h"
+#include "executers/interactive/screens/xterm_warn.h"
 
-int draw_xterm_warn(GM_Term term) {
+Cache make_cache_xterm_warn(GM_Term term, FrameStore store) {
+    Cache cache = make_cache(NULL);
+    GM_TERM_SIZE size = gm_term_get_size(term);
+    
+    char* text = get_localized_string(store->current_locale, LOCALE_SCREEN_XTERM_NOT_SUPPORTED);
+    int len = strlen(text);
+    Tokens lines = get_lines(text, len);
+
+    int max_col = 0;
+    for (int i = 0; i < lines->len; i++) {
+        int len = strlen(lines->data[i]);
+        if (max_col < len) max_col = len; 
+    }
+
+    int x = (size.cols - max_col) / 2;
+    int y = (size.rows - lines->len - 1) / 2;
+
+    destroy_tokens(lines);
+    
+    DrawText dt = make_draw_text(text, len, x, y);
+    // memoize_cache_elem(cache, LOCALE_SCREEN_XTERM_NOT_SUPPORTED, dt);
+    add_cache_elem(cache, LOCALE_SCREEN_XTERM_NOT_SUPPORTED, dt);
+
+    return cache;
+}
+
+void destroy_cache_xterm_warn(Cache cache) {
+    DrawText dt = get_cache_elem(cache, LOCALE_SCREEN_XTERM_NOT_SUPPORTED);
+    destroy_draw_text(dt);
+}
+
+void draw_xterm_warn(GM_Term term, FrameStore store, Cache cache) {
     GM_TERM_SIZE size = gm_term_get_size(term);
 
     gm_attroff(term, GM_RESET);
 
     gm_box(term, 0, 0, size.rows - 1, size.cols - 2);
     
-    char* text = "          The current terminal is not supported.         \n"
-                 "There is no guarantee this program will work as expected.\n"
-                 "                Press any key to continue.               ";
-    int lines = 3;
-    int len = 59;
+    // char* text = "          The current terminal is not supported.         \n"
+    //              "There is no guarantee this program will work as expected.\n"
+    //              "                Press any key to continue.               ";
+    // int lines = 3;
+    // int len = 59;
 
-    int x = (size.cols - len) / 2;
-    int y = (size.rows - lines - 1) / 2;
+    // int x = (size.cols - len) / 2;
+    // int y = (size.rows - lines - 1) / 2;
 
-    gm_printf(term, y, x, text);
+
+    
+    // char* text = get_localized_string(store->current_locale, LOCALE_SCREEN_XTERM_NOT_SUPPORTED);
+    // Tokens lines = get_lines(text, strlen(text));
+
+    // int max_col = 0;
+    // for (int i = 0; i < lines->len; i++) {
+    //     int len = strlen(lines->data[i]);
+    //     if (max_col < len) max_col = len; 
+    // }
+
+    // int x = (size.cols - max_col) / 2;
+    // int y = (size.rows - lines->len - 1) / 2;
+
+    DrawText dl = get_cache_elem(cache, LOCALE_SCREEN_XTERM_NOT_SUPPORTED);
+
+    gm_printf(term, dl->y, dl->x, dl->text);
 }
