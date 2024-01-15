3653a969301bebbcc306eaf6b40e44669e32359d
DarkenLM <rafaelsantosfernandes660@gmail.com>
2024-01-15 02:20:39 +0000

    feat(esi/settings): added locale selection submenu
    - Added locale selection submenu.
    -- Note: Changing locales is not implemented.
    
    - Removed mental sanity.

--- trabalho-pratico/src/executers/interactive/screens/settings.c
+++ trabalho-pratico/src/executers/interactive/screens/settings.c
@@ -3,20 +3,32 @@
 #include "executers/interactive/screens/xterm_warn.h"
 #include "util/math.h"
 
-#define OPTION_INDEX "_option_index"
-#define MAX_OPTION_INDEX 2
-
 #define LANGUAGE_NAME_KEY "language_name"
+#define SELECTION_KEY "_selections"
+#define PAGE_SELECTION_KEY "_page"
+#define OPTION_INDEX_KEY "_option_index"
+#define LOCALES_KEY "_locales"
+#define LOCALE_NAMES_KEY "_locale_names"
+#define LOCALE_SELECTION_KEY "_selections_loc"
+#define LOC_SEL_OPTION_INDEX_KEY "_loc_sel_option_index"
+
+#define MAX_OPTION_INDEX 2
 #define RIGHT_OFFSET 2
 #define TOP_OFFSET 1
+
+enum settings_pages {
+    SETTINGS_PAGE_MAIN,
+    SETTINGS_PAGE_LOC_SEL
+};
+
+void _draw_main_box(GM_Term term, FrameStore store, Cache cache);
+void _draw_loc_sel(GM_Term term, FrameStore store, Cache cache);
 
 #pragma GCC push_options
 #pragma GCC optimize ("O0")
 DrawText make_dt_from_str(char* text, GM_TERM_SIZE size, int offsetX, int offsetY) {
     ssize_t text_len = strlen(text);
     MAX_COLS_AND_ROWS text_mcar = get_max_rows_and_cols(text, text_len);
-    // int x = (size.cols - text_mcar.cols) / 2 + offsetX;
-    // int y = (size.rows - text_mcar.rows - 1) / 2 + offsetY;
     int x = offsetX;
     int y = offsetY;
 
@@ -28,150 +40,252 @@ Cache make_cache_settings(GM_Term term, FrameStore store) {
     Cache cache = make_cache(NULL);
     GM_TERM_SIZE size = gm_term_get_size(term);
 
-    // TITLE
-    char* title = get_localized_string(store->current_locale, LOCALE_SCREEN_SETTINGS_TITLE);
-    DrawText title_dt = make_dt_from_str(title, size, 0, 0);
-    title_dt->x = RIGHT_OFFSET; 
-    title_dt->y = TOP_OFFSET;
-    add_cache_elem(cache, LOCALE_SCREEN_SETTINGS_TITLE, title_dt);
+    // ------- Page Selection -------
+    {
+        int* page_selection = (int*)malloc(sizeof(int));
+        *page_selection = 0;
+        add_cache_elem(cache, PAGE_SELECTION_KEY, page_selection);
 
-    // LANGUAGE KEY
-    char* language = get_localized_string(store->current_locale, LOCALE_SCREEN_SETTINGS_LANGUAGE);
-    DrawText language_dt = make_dt_from_str(language, size, 0, 0);
-    language_dt->x = RIGHT_OFFSET; 
-    language_dt->y = TOP_OFFSET + 2;
-    add_cache_elem(cache, LOCALE_SCREEN_SETTINGS_LANGUAGE, language_dt);
+        // ------- MAIN PAGE -------
+        // TITLE
+        char* title = get_localized_string(store->current_locale, LOCALE_SCREEN_SETTINGS_TITLE);
+        DrawText title_dt = make_dt_from_str(title, size, 0, 0);
+        title_dt->x = RIGHT_OFFSET; 
+        title_dt->y = TOP_OFFSET;
+        add_cache_elem(cache, LOCALE_SCREEN_SETTINGS_TITLE, title_dt);
 
-    // LANGUAGE VALUE
-    char* language_name = get_locale_name(store->current_locale);
-    DrawText language_name_dt = make_dt_from_str(language_name, size, 0, 0);
-    language_name_dt->x = RIGHT_OFFSET + 1 + language_dt->cols; 
-    language_name_dt->y = TOP_OFFSET + 2;
-    add_cache_elem(cache, LANGUAGE_NAME_KEY, language_name_dt);
+        // LANGUAGE KEY
+        char* language = get_localized_string(store->current_locale, LOCALE_SCREEN_SETTINGS_LANGUAGE);
+        DrawText language_dt = make_dt_from_str(language, size, 0, 0);
+        language_dt->x = RIGHT_OFFSET; 
+        language_dt->y = TOP_OFFSET + 2;
+        add_cache_elem(cache, LOCALE_SCREEN_SETTINGS_LANGUAGE, language_dt);
 
-    // FUCKERY KEY
-    char* fuckery = strdup("12345:");
-    DrawText fuckery_dt = make_dt_from_str(fuckery, size, 0, 0);
-    fuckery_dt->x = RIGHT_OFFSET; 
-    fuckery_dt->y = TOP_OFFSET + 3;
-    add_cache_elem(cache, "fuckery", fuckery_dt);
+        // LANGUAGE VALUE
+        char* language_name = get_locale_name(store->current_locale);
+        DrawText language_name_dt = make_dt_from_str(language_name, size, 0, 0);
+        language_name_dt->x = RIGHT_OFFSET + 1 + language_dt->cols; 
+        language_name_dt->y = TOP_OFFSET + 2;
+        add_cache_elem(cache, LANGUAGE_NAME_KEY, language_name_dt);
 
-    // FUCKERY VALUE
-    char* fuckery2 = strdup("67890");
-    DrawText fuckery2_dt = make_dt_from_str(fuckery2, size, 0, 0);
-    fuckery2_dt->x = RIGHT_OFFSET + 1 + fuckery_dt->cols; 
-    fuckery2_dt->y = TOP_OFFSET + 3;
-    add_cache_elem(cache, "fuckery2", fuckery2_dt);
+        // FUCKERY KEY
+        char* fuckery = strdup("12345:");
+        DrawText fuckery_dt = make_dt_from_str(fuckery, size, 0, 0);
+        fuckery_dt->x = RIGHT_OFFSET; 
+        fuckery_dt->y = TOP_OFFSET + 3;
+        add_cache_elem(cache, "fuckery", fuckery_dt);
+
+        // FUCKERY VALUE
+        char* fuckery2 = strdup("67890");
+        DrawText fuckery2_dt = make_dt_from_str(fuckery2, size, 0, 0);
+        fuckery2_dt->x = RIGHT_OFFSET + 1 + fuckery_dt->cols; 
+        fuckery2_dt->y = TOP_OFFSET + 3;
+        add_cache_elem(cache, "fuckery2", fuckery2_dt);
 
 
-    // ------- Align Values -------
-    DrawText keys[] = { language_dt, fuckery_dt };
-    DrawText values[] = { language_name_dt, fuckery2_dt };
-    int pairs = 2;
+        // Align Values
+        DrawText keys[] = { language_dt, fuckery_dt };
+        DrawText values[] = { language_name_dt, fuckery2_dt };
+        int pairs = 2;
 
-    int max_x = 0;
-    for (int i = 0; i < pairs; i++) {
-        if (max_x < keys[i]->x + keys[i]->len) max_x = keys[i]->x + keys[i]->cols;
-    }
-
-    for (int i = 0; i < pairs; i++) values[i]->x = max_x + 2;
-
-    DrawText* selections = (char**)malloc(pairs * sizeof(DrawText));
-    int sel_ind = 0;
-
-    for (int i = 0; i < pairs; i++) {
-        int lines = imax(keys[i]->rows, values[i]->rows);
-        int xs = keys[i]->x;
-        int xf = values[i]->x + values[i]->cols;
-        int length = lines * (xf - xs + 1) + 1;
-
-        char* text = (char*)malloc(length * sizeof(char));
-        // for (int j = 0; j < length; j++) text[j] = ' ';
-        int text_ind = 0;
-        for (int j = 0; j < lines; j++) {
-            for (int k = 0; k < xf - xs; k++) text[text_ind++] = ' ';
-            if (j != lines - 1) text[text_ind++] = '\n';
+        int max_x = 0;
+        for (int i = 0; i < pairs; i++) {
+            if (max_x < keys[i]->x + keys[i]->len) max_x = keys[i]->x + keys[i]->cols;
         }
 
-        text[text_ind] = '\0';
+        for (int i = 0; i < pairs; i++) values[i]->x = max_x + 2;
 
-        DrawText dt = make_draw_text(text, length, xs, keys[i]->y, keys[i]->y + lines, xf - xs);
-        selections[sel_ind++] = dt;
+        DrawText* selections = (DrawText*)malloc(pairs * sizeof(DrawText));
+        int sel_ind = 0;
+
+        for (int i = 0; i < pairs; i++) {
+            int lines = imax(keys[i]->rows, values[i]->rows);
+            int xs = keys[i]->x;
+            int xf = values[i]->x + values[i]->cols;
+            int length = lines * (xf - xs + 1) + 1;
+
+            char* text = (char*)malloc(length * sizeof(char));
+            int text_ind = 0;
+            for (int j = 0; j < lines; j++) {
+                for (int k = 0; k < xf - xs; k++) text[text_ind++] = ' ';
+                if (j != lines - 1) text[text_ind++] = '\n';
+            }
+
+            text[text_ind] = '\0';
+
+            DrawText dt = make_draw_text(text, length, xs, keys[i]->y, keys[i]->y + lines, xf - xs);
+            selections[sel_ind++] = dt;
+        }
+        add_cache_elem(cache, SELECTION_KEY, selections);
+
+        // Option Select
+        int* option_index = (int*)malloc(sizeof(int));
+        *option_index = 0;
+        add_cache_elem(cache, OPTION_INDEX_KEY, option_index);
     }
-    add_cache_elem(cache, "_selections", selections);
+    
+    // ------- Language Selection Screen -------
+    {
+        // Locales
+        GArray* locales = get_all_locales(store->locales);
+        add_cache_elem(cache, LOCALES_KEY, locales);
 
+        char** locnames = (char**)malloc(locales->len * sizeof(char*));
+        for (int i = 0; i < locales->len; i++) locnames[i] = get_locale_name(g_array_index(locales, DataLocale, i));
+        add_cache_elem(cache, LOCALE_NAMES_KEY, locnames);
 
+        int max_col = 0;
+        for (int i = 0; i < locales->len; i++) {
+            int len = strlen(locnames[i]);
+            if (max_col < len) max_col = len;
+        }
 
+        int x = (size.cols - max_col) / 2;
+        int y = (size.rows - locales->len - 1) / 2;
 
+        DrawText* selections = (DrawText*)malloc(locales->len * sizeof(DrawText));
+        int sel_ind = 0;
 
-    // char* title = get_localized_string(store->current_locale, LOCALE_SCREEN_SETTINGS_TITLE);
-    // DrawText title_dt = make_dt_from_str(title, size, RIGHT_OFFSET, TOP_OFFSET);
+        for (int i = 0; i < locales->len; i++) {
+            int len = strlen(locnames[i]);
+            char* text = (char*)malloc(len * sizeof(char));
+            int text_ind = 0;
+            for (int j = 0; j < len; j++) text[text_ind++] = ' ';
 
-    // char* language = get_localized_string(store->current_locale, LOCALE_SCREEN_SETTINGS_LANGUAGE);
-    // DrawText language_dt = make_dt_from_str(language, size, RIGHT_OFFSET, TOP_OFFSET + 2);
+            text[text_ind] = '\0';
 
-    // char* language_name = get_locale_name(store->current_locale);
-    // DrawText language_name_dt = make_dt_from_str(language_name, size, 0, 0);
+            DrawText dt = make_draw_text(text, len, x, y + i, 1, len);
+            selections[sel_ind++] = dt;
+        }
+        add_cache_elem(cache, LOCALE_SELECTION_KEY, selections);
 
-    // char* fuckery = strdup("12345:");
-    // DrawText fuckery_dt = make_dt_from_str(fuckery, size, RIGHT_OFFSET, TOP_OFFSET + 3);
-
-    // char* fuckery2 = strdup("67890");
-    // DrawText fuckery2_dt = make_dt_from_str(fuckery2, size, 0, 0);
-
-    // // ------- Align Values -------
-    // DrawText keys[] = { language_dt, fuckery_dt };
-    // DrawText value[] = { language_name_dt, fuckery2_dt };
-    // int pairs = 2;
-
-    // int max_x = 0;
-    // for (int i = 0; i < pairs; i++) {
-    //     if (max_x < keys[i]->x + keys[i]->len) max_x = keys[i]->x + keys[i]->len;
-    // }
-
-    // for (int i = 0; i < pairs; i++) {
-    //     char* completeText = (char*)malloc(max_x * sizeof(char*));
-    //     strcpy(completeText)
-    // }
-
-
-
-    // ------- Option Select -------
-    int* option_index = (int*)malloc(sizeof(int));
-    *option_index = 0;
-    add_cache_elem(cache, OPTION_INDEX, option_index);
+        // Option Select
+        int* loc_sel_option_index = (int*)malloc(sizeof(int));
+        *loc_sel_option_index = 0;
+        add_cache_elem(cache, LOC_SEL_OPTION_INDEX_KEY, loc_sel_option_index);
+    }
 
     return cache;
 }
 
 void destroy_cache_settings(Cache cache) {
-    DrawText title_dt = get_cache_elem(cache, LOCALE_SCREEN_SETTINGS_TITLE);
-    destroy_draw_text(title_dt);
+    // ------- Page Selection -------
+    int* page_selection = get_cache_elem(cache, PAGE_SELECTION_KEY);
+    free(page_selection);
 
-    DrawText language_dt = get_cache_elem(cache, LOCALE_SCREEN_SETTINGS_LANGUAGE);
-    destroy_draw_text(language_dt);
+    // ------- MAIN PAGE -------
+    {
+        DrawText title_dt = get_cache_elem(cache, LOCALE_SCREEN_SETTINGS_TITLE);
+        destroy_draw_text(title_dt);
 
-    DrawText language_name_dt = get_cache_elem(cache, LANGUAGE_NAME_KEY);
-    destroy_draw_text(language_name_dt);
+        DrawText language_dt = get_cache_elem(cache, LOCALE_SCREEN_SETTINGS_LANGUAGE);
+        destroy_draw_text(language_dt);
 
-    int* option_index = get_cache_elem(cache, OPTION_INDEX);
-    free(option_index);
+        DrawText language_name_dt = get_cache_elem(cache, LANGUAGE_NAME_KEY);
+        destroy_draw_text(language_name_dt);
 
-    DrawText* selections = get_cache_elem(cache, "_selections");
-    for (int i = 0; i < MAX_OPTION_INDEX; i++) destroy_draw_text(selections[i]);
-    free(selections);
+        int* option_index = get_cache_elem(cache, OPTION_INDEX_KEY);
+        free(option_index);
 
-    DrawText fuckery = get_cache_elem(cache, "fuckery");
-    destroy_draw_text(fuckery);
+        DrawText* selections = get_cache_elem(cache, SELECTION_KEY);
+        for (int i = 0; i < MAX_OPTION_INDEX; i++) destroy_draw_text(selections[i]);
+        free(selections);
 
-    DrawText fuckery2 = get_cache_elem(cache, "fuckery2");
-    destroy_draw_text(fuckery2);
+        DrawText fuckery = get_cache_elem(cache, "fuckery");
+        destroy_draw_text(fuckery);
+
+        DrawText fuckery2 = get_cache_elem(cache, "fuckery2");
+        destroy_draw_text(fuckery2);
+    }
+    
+    // ------- Language Selection Screen -------
+    {
+        int* option_index = get_cache_elem(cache, LOC_SEL_OPTION_INDEX_KEY);
+        free(page_selection);
+        
+        // Not freed immediately because it's len is required.
+        GArray* locs = get_cache_elem(cache, LOCALES_KEY);
+
+        char** loc_names = get_cache_elem(cache, LOCALE_NAMES_KEY);
+        for (int i = 0; i < locs->len; i++) free(loc_names[i]);
+        free(loc_names);
+
+        DrawText* selections = get_cache_elem(cache, LOCALE_SELECTION_KEY);
+        for (int i = 0; i < locs->len; i++) destroy_draw_text(selections[i]);
+        free(selections);
+        
+        g_array_free(locs, FALSE);
+    }
 }
 
 void draw_settings(GM_Term term, FrameStore store, Cache cache) {
+    int* page_selection = get_cache_elem(cache, PAGE_SELECTION_KEY);
+
+    _draw_main_box(term, store, cache);
+
+    switch (*page_selection) {
+        // case SETTINGS_PAGE_MAIN: {
+        //     _draw_main_box(term, store, cache);
+        //     break;
+        // }
+        case SETTINGS_PAGE_LOC_SEL: {
+            _draw_loc_sel(term, store, cache);
+        }
+    }
+}
+
+Keypress_Code keypress_settings(GM_Term term, FrameStore store, Cache cache, GM_Key key) {
+    GM_Key ckey = gm_get_canonical_key(key);
+
+    int* page_selection = get_cache_elem(cache, PAGE_SELECTION_KEY);
+    switch (*page_selection) {
+        case SETTINGS_PAGE_MAIN: {
+            int* option_index = get_cache_elem(cache, OPTION_INDEX_KEY);
+
+            if (ckey == GM_KEY_ARROW_DOWN) {
+                (*option_index)++;
+
+                if (*option_index == MAX_OPTION_INDEX) *option_index = 0;
+            } else if (ckey == GM_KEY_ARROW_UP) {
+                (*option_index)--;
+
+                if (*option_index == -1) *option_index = MAX_OPTION_INDEX - 1;
+            } else if (ckey == GM_KEY_ENTER) {
+                switch (*option_index) {
+                    case 0: {
+                        *page_selection = SETTINGS_PAGE_LOC_SEL;
+                        break;
+                    }
+                }
+            }
+            break;
+        }
+        case SETTINGS_PAGE_LOC_SEL: {
+            int* option_index = get_cache_elem(cache, LOC_SEL_OPTION_INDEX_KEY);
+            GArray* locs = get_cache_elem(cache, LOCALES_KEY);
+
+            if (ckey == GM_KEY_ESCAPE) {
+                *page_selection = SETTINGS_PAGE_MAIN;
+                break;
+            } else if (ckey == GM_KEY_ARROW_DOWN) {
+                (*option_index)++;
+
+                if (*option_index == locs->len) *option_index = 0;
+            } else if (ckey == GM_KEY_ARROW_UP) {
+                (*option_index)--;
+
+                if (*option_index == -1) *option_index = locs->len - 1;
+            }
+            break;
+        }
+    }
+}
+
+
+void _draw_main_box(GM_Term term, FrameStore store, Cache cache) {
     GM_TERM_SIZE size = gm_term_get_size(term);
 
-    int* option_index = get_cache_elem(cache, OPTION_INDEX);
+    int* option_index = get_cache_elem(cache, OPTION_INDEX_KEY);
 
     // ------- Localized Strings -------
     DrawText title_dt = get_cache_elem(cache, LOCALE_SCREEN_SETTINGS_TITLE);
@@ -180,15 +294,13 @@ void draw_settings(GM_Term term, FrameStore store, Cache cache) {
     DrawText fuckery = get_cache_elem(cache, "fuckery");
     DrawText fuckery2 = get_cache_elem(cache, "fuckery2");
 
-    DrawText* selections = get_cache_elem(cache, "_selections");
+    DrawText* selections = get_cache_elem(cache, SELECTION_KEY);
 
     DrawText dts[] = { title_dt, language_dt, language_name_dt, fuckery, fuckery2 };
     int dts_len = 5;
 
     // ------- Calculate box width -------
     int max_col = 0;
-    // if (max_col < title_dt->len) max_col = title_dt->len; 
-    // if (max_col < language_dt->len + language_name_dt->len + 1) max_col = language_dt->len + language_name_dt->len + 1;
     for (int i = 0; i < dts_len; i++) {
         if (max_col < dts[i]->x + dts[i]->len) max_col = dts[i]->x + dts[i]->len;
     }
@@ -199,8 +311,7 @@ void draw_settings(GM_Term term, FrameStore store, Cache cache) {
     gm_box(term, 0, 0, TOP_OFFSET + 4, RIGHT_OFFSET + max_col);
 
     // ------- Draw Options -------
-    for (int i = 0; i < 5; i++) {
-        // if (*option_index == (i + 1) / 2 &&  ((i + 1) / 2) == ((i + 2) / 2) && i > 0) {
+    for (int i = 0; i < dts_len; i++) {
         if (*option_index == (i + 1) / 2 - 1 && (i + 1) / 2 == (i + 2) / 2) {
             gm_attron(term, GM_COLOR_PAIR(COLORPAIR_SELECTED));
 
@@ -208,29 +319,42 @@ void draw_settings(GM_Term term, FrameStore store, Cache cache) {
             gm_printf(term, selection->y, selection->x, selection->text);
         }
 
-        // if (((i + 1) / 2) - 1 == *option_index) gm_attron(term, GM_COLOR_PAIR(COLORPAIR_SELECTED));
         gm_printf(term, dts[i]->y, dts[i]->x, dts[i]->text);
-        // if (((i + 1) / 2) - 1 == *option_index) gm_attroff(term, GM_COLOR_PAIR(COLORPAIR_SELECTED));
 
         if (*option_index == (i + 1) / 2 - 1 && (i + 1) / 2 != (i + 2) / 2) 
             gm_attroff(term, GM_COLOR_PAIR(COLORPAIR_SELECTED));
     }
 }
 
-Keypress_Code keypress_settings(GM_Term term, FrameStore store, Cache cache, GM_Key key) {
-    GM_Key ckey = gm_get_canonical_key(key);
-    int* option_index = get_cache_elem(cache, OPTION_INDEX);
+void _draw_loc_sel(GM_Term term, FrameStore store, Cache cache) {
+    GM_TERM_SIZE size = gm_term_get_size(term);
 
-    if (ckey == GM_KEY_ARROW_DOWN) {
-        (*option_index)++;
+    int* option_index = get_cache_elem(cache, LOC_SEL_OPTION_INDEX_KEY);
+    GArray* locs = get_cache_elem(cache, LOCALES_KEY);
+    char** loc_names = get_cache_elem(cache, LOCALE_NAMES_KEY);
 
-        if (*option_index == MAX_OPTION_INDEX) *option_index = 0;
-    }
+    DrawText* selections = get_cache_elem(cache, LOCALE_SELECTION_KEY);
 
-    if (ckey == GM_KEY_ARROW_UP) {
-        (*option_index)--;
+    gm_attroff(term, GM_RESET);
 
-        if (*option_index == -1) *option_index = MAX_OPTION_INDEX - 1;
+    // ------- Draw Border Box -------
+    gm_box(
+        term, 
+        selections[0]->y - 1, 
+        selections[0]->x - 1, 
+        selections[locs->len - 1]->y + 1, 
+        selections[0]->x + selections[0]->cols
+    );
+
+    for (int i = 0; i < locs->len; i++) {
+        if (*option_index == i) gm_attron(term, GM_COLOR_PAIR(COLORPAIR_SELECTED));
+
+        DrawText selection = selections[i];
+        gm_printf(term, selection->y, selection->x, selection->text);
+        gm_printf(term, selection->y, selection->x, loc_names[i]);
+
+        if (*option_index == i) gm_attroff(term, GM_COLOR_PAIR(COLORPAIR_SELECTED));
     }
 }
+
 #pragma GCC pop_options
