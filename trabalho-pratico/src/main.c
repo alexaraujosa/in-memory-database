63fd4669376f1858701eee95281a26b1178fe72d
DarkenLM <rafaelsantosfernandes660@gmail.com>
2024-01-16 16:07:21 +0000

    feat(main): added main required since ALL THE WAY BACK AT a6e2609b
    - I REALLY needed to sleep, huh?
    
    - Removed mental sanity.

--- trabalho-pratico/src/main.c
+++ trabalho-pratico/src/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <time.h>
 #include "executers/batch.h"
-#include "executers/interactive.h"
+#include "executers/interactive/interactive.h"
+#include "data/data.h"
+#include "data/locale.h"
 
 #define RESET   "\x1b[0m"
 #define RED     "\x1b[31m"
@@ -9,16 +11,27 @@
 #define YELLOW  "\x1b[33m"
 #define BLUE    "\x1b[34m"
 
+#pragma GCC push_options
+#pragma GCC optimize ("O0")
 int main(int argc, char const *argv[]) {
     clock_t start_time = clock();
+
+    // Initialize asset path, further calls should use NULL.
+    char* asset_path = get_asset_dir("assets");
+    DataLocales locales = load_locales();
 
     if(argc > 2) {
         batch(argv[1], argv[2]);
     } else {
         // printf("Interactive mode not implemented.\n");
         // exit(EXIT_FAILURE);
-        interactive();
+        interactive(locales);
     }
+
+    // Cleanup
+    destroy_locales(locales);
+    g_string_free(get_cwd(), TRUE);
+    free(asset_path);
     
     clock_t end_time = clock();
     double elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
@@ -26,3 +39,4 @@ int main(int argc, char const *argv[]) {
 
     return 0;
 }
+#pragma GCC pop_options
