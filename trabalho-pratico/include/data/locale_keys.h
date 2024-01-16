fb6f799d256ec7fead77fe1cace50825dec17ad4
DarkenLM <rafaelsantosfernandes660@gmail.com>
2024-01-16 16:05:05 +0000

    feat(data/locale): added localization keys for 4329042c
    - Removed mental sanity.

--- trabalho-pratico/include/data/locale_keys.h
+++ trabalho-pratico/include/data/locale_keys.h
@@ -10,8 +10,20 @@
 #define DATA_LOCALE_EN_US "en_US"
 #define DATA_LOCALE_PT_PT "pt_PT"
 
+// ------- Miscellaneous ------
+#define LOCALE_EXIT "exit"
+
+// ------- Xterm Warn -------
 #define LOCALE_SCREEN_XTERM_NOT_SUPPORTED "screen.xterm.not_supported"
+
+// ------- Settings -------
 #define LOCALE_SCREEN_SETTINGS_TITLE "screen.settings"
+#define LOCALE_SCREEN_SETTINGS_TITLE_PROPERCASE "screen.settings.propercase"
 #define LOCALE_SCREEN_SETTINGS_LANGUAGE "screen.settings.language"
+
+// ------- Main Menu -------
+#define LOCALE_SCREEN_MAIN_MENU_TITLE "screen.main_menu"
+#define LOCALE_SCREEN_MAIN_EXECUTE_QUERY "screen.main_menu.execute_query"
+
 
 #endif
