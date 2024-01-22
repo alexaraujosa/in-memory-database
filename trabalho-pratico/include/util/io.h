067775eb3172f143fbd15c3311419c7999674517
DarkenLM <rafaelsantosfernandes660@gmail.com>
2024-01-22 21:26:30 +0000

    fix(util/io): FUCK OFF NCURSES
    - Seriously, FUCK OFF.
    
    - Removed mental sanity.

--- trabalho-pratico/include/util/io.h
+++ trabalho-pratico/include/util/io.h
@@ -1,3 +1,11 @@
+/**
+ * @file io.h
+ * 
+ * IO
+ * 
+ * THis module defines miscellaeous functions related to IO operations. 
+ */
+
 #ifndef TP_UTIL_IO_H
 #define TP_UTIL_IO_H
 
@@ -10,8 +18,22 @@
 #include "util/error.h"
 #include "debug.h"
 
+// FUCK OFF, NCURSES
+#undef lines
+
+/**
+ * @brief The filesystem path separator for Unix systems. 
+ */
 #define FS_PATH_SEPARATOR '/'
 
+/**
+ * @brief Opens a file stream and exits the program if it failed.
+ * 
+ * @param filename The path of the file to open.
+ * @param mode The mode to open this file on.
+ * 
+ * @return The file stream for the file.
+ */
 #define OPEN_FILE(filename, mode) ({\
     FILE* stream = fopen(filename, mode);\
     if (stream == NULL) {\
@@ -21,6 +43,11 @@
     stream;\
 })
 
+/**
+ * @brief CLoses a file stream and exits the program if it failed.
+ * 
+ * @param stream The file stream to close.
+ */
 #define CLOSE_FILE(stream) ({\
     if (fclose(stream) == EOF) {\
         fprintf(stderr, "Error closing file.\n %s\n", strerror(errno));\
@@ -28,44 +55,68 @@
     }\
 })
 
-/*
- * Concatenates an array of strings into a single filesystem path.
+/**
+ * @brief Concatenates an array of strings into a single filesystem path.
+ * 
+ * @param len The number of path parts to join.
+ * @param ... The path parts to be joined.
+ * 
+ * @return The joint path (not resolved).
  */
-// char* join_paths(char** paths, int len);
 char* join_paths(int len, ...);
 
-/*
- * Returns a GArray containing the full path of all files within a directory.
+/**
+ * @brief Returns a ::GArray containing the full path of all files within a directory.
+ * 
+ * @param path The absolute path to the directory.
+ * 
+ * @return A ::GArray containing the path of every file within the directory.
  */
 GArray* get_files(char* path);
 
-/*
- * Returns a GArray containing the full path of all subdirectories within a directory.
+/**
+ * @brief Returns a GArray containing the full path of all subdirectories within a directory.
+ * 
+ * @param path The absolute path to the directory.
+ * 
+ * @return A ::GArray containing the path of every subdirectory within the directory.
  */
 GArray* get_subdirs(char* path);
 
-/*
- * Returns the Current Working Directory of this program (CWD).
+/**
+ * @brief Returns the Current Working Directory of this program (CWD).
+ * 
+ * @note This function is memoized.
+ * 
+ * @return The CWD of the program.
  */
 GString* get_cwd();
 
-/*
- * Returns the directory part of a path. If the path is relative, it is resolved against the CWD.
+/**
+ * @brief Returns the directory part of a path. If the path is relative, it is resolved against the CWD.
+ * 
+ * @return The dirname of the path.
  */
 char* dirname_absolute(char* path);
 
-/*
- * Resolves the path against the CWD. If the path is absolute, the path itself is return.
+/**
+ * @brief Resolves the path against the CWD. If the path is absolute, the path itself is returned.
+ * 
+ * @return The resolved path.
  */
 char* resolve_to_cwd(char* path);
 
-/*
- * Attemps to skip N lines of a given FILE.
+/**
+ * @brief Attemps to skip N lines of a given FILE.
+ * 
+ * @return 0 if everything succeeded, 1 otherwise.
  */
-int skip_n_lines(FILE* file, int lines);
+int skip_n_lines(FILE* file, int str_lines);
 
-/*
- * Determines wheather a path is absolute or not.
+/**
+ * @brief Determines wheather a path is absolute or not.
+ * 
+ * @return 0 if the path is not absolute, 1 otherwise.
  */
 int is_path_absolute(char* path);
 
