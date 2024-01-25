210cdac3cdadfa055fa36315d571d4fa30373fcd
DarkenLM <rafaelsantosfernandes660@gmail.com>
2024-01-25 23:46:28 +0000

    feat(assets/lang): added localization keys. translated pt_PT
    - Added missing localization keys.
    - Translated entire project into pt_PT.

--- trabalho-pratico/include/data/locale_keys.h
+++ trabalho-pratico/include/data/locale_keys.h
@@ -12,6 +12,8 @@
 
 // ------- Miscellaneous ------
 #define LOCALE_EXIT "exit"
+#define LOCALE_ACTIVE_UPPERCASE "active_UC"
+#define LOCALE_INACTIVE_UPPERCASE "inactive_UC"
 #define LOCALE_SCREEN_HELP_TOOLTIP "screen.help_tooltip"
 
 // ------- Xterm Warn -------
@@ -21,6 +23,7 @@
 #define LOCALE_SCREEN_SETTINGS_TITLE "screen.settings"
 #define LOCALE_SCREEN_SETTINGS_TITLE_PROPERCASE "screen.settings.propercase"
 #define LOCALE_SCREEN_SETTINGS_LANGUAGE "screen.settings.language"
+#define LOCALE_SCREEN_SETTINGS_ARA "screen.settings.ara"
 
 // ------- Main Menu -------
 #define LOCALE_SCREEN_MAIN_MENU_TITLE "screen.main_menu"
@@ -39,8 +42,25 @@
 #define LOCALE_SCREEN_QUERY_OUTPUT_HELP "screen.query_output.help"
 
 // ------- Query Selection -------
+#define LOCALE_SCREEN_QUERY_SELECTION_TITLE  "screen.query_selection.title"
+
+#define LOCALE_SCREEN_QUERY_SELECTION_QUERY1  "screen.query_selection.query1"
+#define LOCALE_SCREEN_QUERY_SELECTION_QUERY2  "screen.query_selection.query2"
+#define LOCALE_SCREEN_QUERY_SELECTION_QUERY3  "screen.query_selection.query3"
+#define LOCALE_SCREEN_QUERY_SELECTION_QUERY4  "screen.query_selection.query4"
+#define LOCALE_SCREEN_QUERY_SELECTION_QUERY5  "screen.query_selection.query5"
+#define LOCALE_SCREEN_QUERY_SELECTION_QUERY6  "screen.query_selection.query6"
+#define LOCALE_SCREEN_QUERY_SELECTION_QUERY7  "screen.query_selection.query7"
+#define LOCALE_SCREEN_QUERY_SELECTION_QUERY8  "screen.query_selection.query8"
+#define LOCALE_SCREEN_QUERY_SELECTION_QUERY9  "screen.query_selection.query9"
+#define LOCALE_SCREEN_QUERY_SELECTION_QUERY10 "screen.query_selection.query10"
+
+#define LOCALE_SCREEN_QUERY_SELECTION_ARG1 "screen.query_selection.arg1"
+#define LOCALE_SCREEN_QUERY_SELECTION_ARG2 "screen.query_selection.arg2"
+#define LOCALE_SCREEN_QUERY_SELECTION_ARG3 "screen.query_selection.arg3"
+#define LOCALE_SCREEN_QUERY_SELECTION_PRETTY_FORMAT "screen.query_selection.pretty_format"
+
 #define LOCALE_SCREEN_QUERY_SELECTION_HELP "screen.query_selection.help"
-#define _LOCALE_SCREEN_QUERY_SELECTION_HELP_QUERY   "screen.query_selection.help.query" // Iterative completion
 #define LOCALE_SCREEN_QUERY_SELECTION_HELP_QUERY_1  "screen.query_selection.help.query1"
 #define LOCALE_SCREEN_QUERY_SELECTION_HELP_QUERY_2  "screen.query_selection.help.query2"
 #define LOCALE_SCREEN_QUERY_SELECTION_HELP_QUERY_3  "screen.query_selection.help.query3"
@@ -51,5 +71,24 @@
 #define LOCALE_SCREEN_QUERY_SELECTION_HELP_QUERY_8  "screen.query_selection.help.query8"
 #define LOCALE_SCREEN_QUERY_SELECTION_HELP_QUERY_9  "screen.query_selection.help.query9"
 #define LOCALE_SCREEN_QUERY_SELECTION_HELP_QUERY_10 "screen.query_selection.help.query10"
+
+// ------- Query Errors -------
+#define LOCALE_QUERIES_ARGC1 "queries.verify.argc1"
+#define LOCALE_QUERIES_ARGC2 "queries.verify.argc2"
+#define LOCALE_QUERIES_ARGC3 "queries.verify.argc3"
+#define LOCALE_QUERIES_ARG2_SKIP "queries.verify.arg2_skip"
+#define LOCALE_QUERIES_ARG3_SKIP "queries.verify.arg3_skip"
+#define LOCALE_QUERIES_TOO_MANY_ARGUMENTS "queries.verify.too_many_arguments"
+#define LOCALE_QUERIES_QUERY2_SECOND "queries.verify.query2.second"
+#define LOCALE_QUERIES_QUERY3_INVALID_ID "queries.verify.query3.invalid_id"
+#define LOCALE_QUERIES_QUERY5_INVALID_NAME "queries.verify.query5.invalid_name"
+#define LOCALE_QUERIES_QUERY5_INVALID_BEGIN_DATE "queries.verify.query5.invalid_begin_date"
+#define LOCALE_QUERIES_QUERY5_INVALID_END_DATE "queries.verify.query5.invalid_end_date"
+#define LOCALE_QUERIES_QUERY6_YEAR_NAN "queries.verify.query6.year_nan"
+#define LOCALE_QUERIES_QUERY6_N_NAN "queries.verify.query6.n_nan"
+#define LOCALE_QUERIES_QUERY9_PREFIX "queries.verify.query9.prefix"
+#define LOCALE_QUERIES_QUERY10_YEAR "queries.verify.query10.year"
+#define LOCALE_QUERIES_QUERY10_MONTH "queries.verify.query10.month"
+#define LOCALE_QUERIES_ERROR_EOM "queries.error.empty_or_malformed"
 
 #endif
